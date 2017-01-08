af0aacd483678518ec4543099ff1cefe8d2e631e
Kevin Ansfield <kevin@lookingsideways.co.uk>
2017-01-08 14:05:42 +0000

    add authenticated requests pipeline

--- web/router.ex
+++ web/router.ex
@@ -1,10 +1,19 @@
 defmodule Peepchat.Router do
   use Peepchat.Web, :router
 
+  # unauthenticated requests
   pipeline :api do
-    plug :accepts, ["json"]
+    plug :accepts, ["json", "json-api"]
   end
 
+  # authenticated requests
+  pipeline :api_auth do
+    plug :accepts, ["json", "json-api"]
+    plug Guardian.Plug.VerifyHeader
+    plug Guardian.Plug.LoadResource
+  end
+
+  # unauthenticated requests
   scope "/api", Peepchat do
     pipe_through :api
 
@@ -13,5 +22,12 @@ defmodule Peepchat.Router do
 
     # login
     post "/token", SessionController, :create, as: :login
+  end
+
+  # authenticated requests
+  scope "/api", Peepchat do
+    pipe_through :api_auth
+
+    get "/user/current", UserController, :current
   end
 end
